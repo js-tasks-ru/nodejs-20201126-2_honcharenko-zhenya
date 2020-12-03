af1a6066ed487a6f26ba9666c1282fa4c3bdf95b
yevhenii honcharenko <yevhenii.honcharenko@x2sy.com>
2020-12-03 22:22:09 +0200

    finish 03/01

--- 03-streams/01-limit-size-stream/LimitSizeStream.js
+++ 03-streams/01-limit-size-stream/LimitSizeStream.js
@@ -2,12 +2,21 @@ const stream = require('stream');
 const LimitExceededError = require('./LimitExceededError');
 
 class LimitSizeStream extends stream.Transform {
+  #limit;
+  #size = 0;
+
   constructor(options) {
     super(options);
+    this.#limit = options.limit;
   }
 
   _transform(chunk, encoding, callback) {
-
+    this.#size += Buffer.byteLength(chunk);
+    if (this.#size > this.#limit) {
+      callback(new LimitExceededError());
+    } else {
+      callback(null, chunk);
+    }
   }
 }
 
