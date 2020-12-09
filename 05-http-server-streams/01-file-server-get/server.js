c626fa50749ecbd589b963b76f9ed9dd8b37e46e
yevhenii honcharenko <yevhenii.honcharenko@x2sy.com>
2020-12-09 11:05:04 +0200

    finish 05/01

--- 05-http-server-streams/01-file-server-get/server.js
+++ 05-http-server-streams/01-file-server-get/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -11,7 +12,42 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'GET':
+      if (pathname.includes('/')) {
+        res.statusCode = 400;
+        res.end('No included directory');
+      }
 
+      const isFileExist = fs.existsSync(filepath);
+      if (!isFileExist) {
+        res.statusCode = 404;
+        res.end('File not exist');
+      }
+
+      const readStream = fs.createReadStream(filepath);
+
+      readStream.on('readable', () => {
+        let chunk = readStream.read();
+        while (null !== chunk) {
+          res.write(chunk);
+          chunk = readStream.read();
+        }
+      });
+
+      readStream.once('end', () => {
+        res.statusCode = 200;
+        res.end();
+      });
+
+      readStream.on('error', (err) => {
+        res.statusCode = 500;
+        res.end(err);
+      });
+
+      res.on('end', () => {
+        if (!res.complete) {
+          readStream.destroy();
+        }
+      });
       break;
 
     default:
