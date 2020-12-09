c570bf4dfa5859f49aca75185f80fdcf0efdac24
YHoncharenko <YHoncharenko@cloudroute.com>
2020-12-09 20:41:17 +0200

    start add 05/02

--- 05-http-server-streams/02-file-server-post/server.js
+++ 05-http-server-streams/02-file-server-post/server.js
@@ -1,6 +1,8 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const LimitSizeStream = require('./LimitSizeStream');
 
 const server = new http.Server();
 
@@ -11,7 +13,48 @@ server.on('request', (req, res) => {
 
   switch (req.method) {
     case 'POST':
+      const isFileExist = fs.existsSync(filepath);
 
+      if (isFileExist) {
+        res.statusCode = 409;
+        res.end('File exist');
+        return;
+      }
+
+      const onData = (chunk) => {
+        writeStream.write(chunk);
+      };
+
+      const limitStream = new LimitSizeStream({limit: 2 ** 20, encoding: 'utf-8'});
+      const writeStream = fs.createWriteStream(filepath);
+
+      writeStream.on('finish', () => {
+        res.statusCode = 200;
+        res.end('File uploaded correct!');
+      });
+
+      limitStream.on('data', onData);
+
+      limitStream.on('end', () => {
+        writeStream.end();
+      });
+
+      limitStream.on('error', (err) => {
+        res.statusCode = 413;
+        res.end('File too big');
+      });
+
+      req.on('data', (chunk) => {
+        limitStream.write(chunk);
+      });
+      req.on('end', () => {
+        limitStream.end();
+      });
+      req.on('abort', () => {
+        limitStream.destroy();
+        writeStream.destroy();
+      });
+      // res.end('DO LATER');
       break;
 
     default:
