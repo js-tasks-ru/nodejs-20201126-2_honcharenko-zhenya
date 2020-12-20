3b7e244f79b4ce75419cca3fb714467ceb627d0b
yevhenii honcharenko <yevhenii.honcharenko@x2sy.com>
2020-12-20 22:15:09 +0200

    finish 08/02

--- 08-authentication/02-oauth/libs/strategies/authenticate.js
+++ 08-authentication/02-oauth/libs/strategies/authenticate.js
@@ -1,3 +1,20 @@
-module.exports = function authenticate(strategy, email, displayName, done) {
-  done(null, false, `функция аутентификации с помощью ${strategy} не настроена`);
+const User = require('../../models/User');
+
+module.exports = async function authenticate(strategy, email, displayName, done) {
+  if (!email) {
+    return done(null, false, 'Не указан email');
+  }
+  let user = await User.findOne({email});
+  if (!user) {
+    user = new User({
+      displayName,
+      email,
+    });
+    const err = user.validateSync();
+    if (err) {
+      return done(err);
+    }
+    await user.save();
+  }
+  done(null, user);
 };
