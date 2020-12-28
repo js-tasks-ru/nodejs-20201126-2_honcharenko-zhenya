3c86437d0cf399ac21a2b91de6173492c44ba86b
yevhenii honcharenko <yevhenii.honcharenko@x2sy.com>
2020-12-28 11:03:02 +0200

    finish 10/01

--- 10-websockets/01-chat/controllers/messages.js
+++ 10-websockets/01-chat/controllers/messages.js
@@ -1,5 +1,7 @@
 const Message = require('../models/Message');
+const mapMessages = require('../mappers/messages');
 
 module.exports.messageList = async function messages(ctx, next) {
-  ctx.body = {messages: []};
+  const messages = await Message.find({chat: ctx.user._id});
+  ctx.body = {messages: messages.map(mapMessages)};
 };
--- 10-websockets/01-chat/socket.js
+++ 10-websockets/01-chat/socket.js
@@ -7,11 +7,30 @@ function socket(server) {
   const io = socketIO(server);
 
   io.use(async function(socket, next) {
+    const {token} = socket.handshake.query;
+    if (!token) {
+      next(new Error('anonymous sessions are not allowed'));
+      return;
+    }
+    const sessionDB = await Session.findOne({token}).populate('user');
+    if (!sessionDB) {
+      next(new Error('wrong or expired session token'));
+      return;
+    }
+    socket.user = sessionDB.user;
     next();
   });
 
   io.on('connection', function(socket) {
-    socket.on('message', async (msg) => {});
+    socket.on('message', async (msg) => {
+      const message = new Message({
+        date: new Date(),
+        text: msg,
+        chat: socket.user._id,
+        user: socket.user.displayName,
+      });
+      await message.save();
+    });
   });
 
   return io;
